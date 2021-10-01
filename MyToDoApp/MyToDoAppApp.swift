a79bf5a9b018bfc7c2a98c133d29aaf88b4dd7ea
Luka <vujnovacluka89@gmail.com>
2021-10-01 19:31:02 +0200

    Initial Commit

---
+++ MyToDoApp/MyToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoAppApp.swift
+//  MyToDoApp
+//
+//  Created by Luka Vujnovac on 01.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct MyToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
